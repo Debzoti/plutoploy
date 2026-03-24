6ebc28b7bf6728e31065187fb9c1b3ed14d6ec0d
debjyoti <debjyotisarkar929@gmail.com>
2026-03-24 17:43:43 +0530

    updateed pulllimage

--- backend/handlers/container.handler.ts
+++ backend/handlers/container.handler.ts
@@ -1,4 +1,8 @@
+import { rejects } from 'assert';
+import { error, log } from 'console';
+import { resolve } from 'dns';
 import Docker from 'dockerode';
+import { logger } from 'hono/logger';
 import Stream from 'stream';
 const docker = new Docker();
 
@@ -9,17 +13,43 @@ const portBindings = {
 //pull the image
 const pullImage = async (imagename : string) : Promise<void> => {
     if(imagename == '') return ;
-    await docker.pull(imagename, function(err : Error, stream : NodeJS.ReadStream ) {
-        docker.modem.followProgress(stream , onProgress, onFinished);
+
+    return new Promise((resolve,reject) =>{
+
+        docker.pull(imagename, function(err : Error, stream : NodeJS.ReadStream ) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            
+            docker.modem.followProgress(stream , onProgress, onFinished);
+
+
+            function onFinished(err: Error | null)  : void{
+                if (err) {
+                    console.error("error in pulling image");
+                    reject(err)
+                    
+                } else {
+                    console.log("image pullled succesfully");
+                    resolve();
+                }
+            }
+            function onProgress(event : any){
+                if(event.status == "Dowloading"){
+                    process.stdout.clearLine(0);
+                    process.stdout.cursorTo(0);
+                    process.stdout.write(event.progress);
+                }else{
+                    process.stdout.clearLine(0);
+                    process.stdout.cursorTo(0);
+                    process.stdout.write(event.status + "\n");
+                }
+            }
+        }) 
+        
     })
 
-    function onFinished() : [] {
-        createContainer();
-    }
-
-    function onProgress(){
-
-    }
 
 }
 
@@ -32,21 +62,26 @@ const pullImage = async (imagename : string) : Promise<void> => {
  * @param deployid
  */
 
-    const createContainer = async (repoUrl : string, imageName: string, deployid : string) =>{
+    const createContainer = async (repoUrl : string, imageName: string, buildId : string) : Promise<Docker.Container> =>{
         //habdle repoerl amnndimage name uissues
 
 
-        const container = docker.createContainer({
-            Image?: imageName,
+        const container = await docker.createContainer({
+            Image: imageName,
+            name : `build ${buildId}`,
             AttachStderr: true,
             AttachStdin: false,
-            AttachStdout : true,
-            Tty:true,
-            ExposedPorts : {'80/tcp' : {}},
-            HostConfig : {
+            AttachStdout: true,
+            Tty: true,
+            Cmd : [],
+            ExposedPorts: { '80/tcp': {} },
+            HostConfig: {
+                PidsLimit:100,
                 PortBindings: portBindings
+                // for ci cd memory and cpu credentials
             },
-            // for ci cd memory and cpu credentials
-            name: 'test-Container',
+            
         })
+
+        return container;
     }
